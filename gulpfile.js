779c1e5c57c9e2e303a44a41ebab30a9c012adbf
CVO-Strongberry <cvo.strongberry@gmail.com>
2018-02-09 00:13:48 +0200

    Added task of compilling less in development mode.

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ const gulp = require('gulp'),
       gulp_plumber = require('gulp-plumber'),
       gulp_sourcemaps = require('gulp-sourcemaps'),
       gulp_sass = require('gulp-sass'),
-      gulp_stylus = require('gulp-stylus');
+      gulp_stylus = require('gulp-stylus'),
+      gulp_less = require('gulp-less');
 
 const path = {
     sass: './src/sass/**/*.scss',
     stylus: './src/stylus/**/*.styl',
+    less: './src/less/**/*.less',
     css: './dist/css'
 }
 
@@ -24,6 +26,15 @@ gulp.task('stylus:development', () => {
         .pipe(gulp_plumber())
         .pipe(gulp_sourcemaps.init())
         .pipe(gulp_stylus())
+        .pipe(gulp_sourcemaps.write('.'))
+        .pipe(gulp.dest(path.css));
+});
+
+gulp.task('less:development', () => {
+    gulp.src(path.less)
+        .pipe(gulp_plumber())
+        .pipe(gulp_sourcemaps.init())
+        .pipe(gulp_less())
         .pipe(gulp_sourcemaps.write('.'))
         .pipe(gulp.dest(path.css));
 });
