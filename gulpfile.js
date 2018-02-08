651c329fbe6cf0fd67eeef24eab0db66ccdbb1c8
CVO-Strongberry <cvo.strongberry@gmail.com>
2018-02-08 23:56:24 +0200

    Add task of compiling stylus in development mode.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp'),
       gulp_plumber = require('gulp-plumber'),
       gulp_sourcemaps = require('gulp-sourcemaps'),
-      gulp_sass = require('gulp-sass');
+      gulp_sass = require('gulp-sass'),
+      gulp_stylus = require('gulp-stylus');
 
 const path = {
     sass: './src/sass/**/*.scss',
+    stylus: './src/stylus/**/*.styl',
     css: './dist/css'
 }
 
@@ -13,6 +15,15 @@ gulp.task('sass:development', () => {
         .pipe(gulp_plumber())
         .pipe(gulp_sourcemaps.init())
         .pipe(gulp_sass({outputStyle: 'expanded'}))
+        .pipe(gulp_sourcemaps.write('.'))
+        .pipe(gulp.dest(path.css));
+});
+
+gulp.task('stylus:development', () => {
+    gulp.src(path.stylus)
+        .pipe(gulp_plumber())
+        .pipe(gulp_sourcemaps.init())
+        .pipe(gulp_stylus())
         .pipe(gulp_sourcemaps.write('.'))
         .pipe(gulp.dest(path.css));
 });
